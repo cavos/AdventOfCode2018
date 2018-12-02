84c9df9a1254a1ad38f387ed1e0bb1f024365e81
cavos <kkwiecinski@outlook.com>
2018-12-02 14:22:59 +0100

    day01

---
+++ src/day01.rs
@@ -0,0 +1,34 @@
+// mod day01 {
+
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+use std::collections::BTreeSet;
+
+pub fn solve_part1(){
+    let buffered = BufReader::new(File::open("./input/day01.txt").unwrap());
+    let result = buffered.lines().map(|l| l.unwrap()).fold(0i32, |freq, x| freq + x.parse::<i32>().unwrap());
+    println!("Day 01 pt1 : {:}", result);
+}
+
+pub fn solve_part2() {
+    let buffered = BufReader::new(File::open("./input/day01.txt").unwrap());
+    let freqs : Vec<i32> = buffered.lines().map(|l| l.unwrap().parse::<i32>().unwrap()).collect();
+
+    let mut found = false;
+    let mut freq = 0;
+    let mut it = freqs.iter();
+    let mut sum_freqs : BTreeSet<i32> = BTreeSet::new();
+    while !found {
+        let f = it.next();
+        if f.is_some() {
+            freq += f.unwrap();
+            if !sum_freqs.insert(freq) {
+                found = true;
+            }
+        } else {
+            it = freqs.iter();
+        }
+    }
+    println!("Day 01 pt2 : {:}", freq);
+}
+// }
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// extern crate day01;
+mod day01;
+
+
+fn main() {
+    println!("Hello, world!");
+
+    day01::solve_part1();
+    day01::solve_part2();
+}
